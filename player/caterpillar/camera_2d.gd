67581179b1aafafa6b01b9057a47aeafded4e6ec
JasperGMDV <sanikar3456@gmail.com>
2026-05-02 02:11:51 +0700

    caterpillar update

--- player/caterpillar/camera_2d.gd
+++ player/caterpillar/camera_2d.gd
@@ -2,16 +2,16 @@ extends Camera2D
 @onready var caterpillar: Caterpillar = $"../caterpillar_container/caterpillar"
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("caterpillar"):
-		global_position = Vector2(442, 434)
+		var tw = create_tween()
+		tw.tween_property(self, "global_position", Vector2(442, 434),0.2)
+		
+
+func _on_second_body_entered(body: Node2D) -> void:
+	if body.is_in_group("caterpillar"):
+		var tw = create_tween()
+		tw.tween_property(self, "global_position", Vector2(815.0, 319.0),0.2)
--- player/caterpillar/stretch_up.gd
+++ player/caterpillar/stretch_up.gd
@@ -4,29 +4,36 @@ extends CaterpillarState
 
 var time = 0.0
 var stretch_finish = false
+var anim_finish = false
 var bumped = false
 func enter_state():
 	time = 0.0
 	stretch_finish = false
 	bumped = false
+	anim_finish = false
 	player.animator.play("vert")
 	player.head.reparent(player.rsegment)
 	player.head.position = Vector2.ZERO
 	player.head.offset.x = 111
 	player.head.flip_h = false
+	await player.animator.animation_finished
+	anim_finish = true
 func update(delta: float):
+	if !stretch_finish:
+		player.handle_gravity(delta)
+	if !anim_finish:
+		return
 	time += delta
-
 	if time >= player.stretch_limit:
 		stretch_finish = true
 	if ray_right.is_colliding():
 		if ray_right.get_collider().is_in_group("wall"):
 			stretch_finish = true
 			bumped = true
-	player.handle_gravity(delta)
+	
 	if Input.is_action_pressed("ui_up") and !stretch_finish:
 		player.scale.x += 0.1 *delta*player.speed
-	elif Input.is_action_just_released("ui_up"):
+	elif Input.is_action_just_released("ui_up") or !Input.is_action_pressed("ui_up"):
 		stretch_finish = true
 	if stretch_finish:
 		player.change_state(states.move_up)
