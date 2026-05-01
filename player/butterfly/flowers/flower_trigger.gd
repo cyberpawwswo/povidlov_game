fba20ca83d6824eae203c505bf89987326e4055b
cyberpawwswo <20test121@gmail.com>
2026-05-01 14:31:20 +0300

    add flowers pictuters

--- player/butterfly/flowers/flower_trigger.gd
+++ player/butterfly/flowers/flower_trigger.gd
@@ -5,7 +5,9 @@ var count := 0
 var start_tween: Tween
 
 func _on_trigger_animation_body_entered(body: Node2D) -> void:
-	if body is Butterfly:
+	if body is Butterfly and not $ProgressBar.visible:
+		$ProgressBar.visible = true
+
 		start_tween = create_tween()
 		start_tween.tween_property($ProgressBar, 'value', 100, 2)
 		#tween.custom_step(0.01)
@@ -17,8 +19,12 @@ func _on_trigger_animation_body_exited(body: Node2D) -> void:
 		var tween = create_tween()
 		tween.tween_property($ProgressBar, 'value', 0, 0.5)
 		#tween.custom_step(0.01)
-		#tween.connect('finished', tween_finished.bind(body))
+		tween.connect('finished', tween_reset)
+
+func tween_reset():
+	$ProgressBar.visible = false
 
 func tween_finished(player: Butterfly):
 	player.chroma_point += get_parent().chroma_point
+	$ProgressBar.visible = false
 	print("as")
