1852d5b078b8bea9d0bbe74e65697b652f3e0653
cyberpawwswo <20test121@gmail.com>
2026-05-02 21:39:14 +0300

    add particle for active flowers

--- player/butterfly/flowers/flower_trigger.gd
+++ player/butterfly/flowers/flower_trigger.gd
@@ -4,8 +4,16 @@ var count := 0
 
 var start_tween: Tween
 
+var is_active := true
+
+func _ready() -> void:
+	global_rotation = 0
+	for i in get_children():
+		if i is CollisionShape2D:
+			i.reparent($TriggerAnimation)
+
 func _on_trigger_animation_body_entered(body: Node2D) -> void:
-	if body is Butterfly and not $ProgressBar.visible:
+	if body is Butterfly and not $ProgressBar.visible and is_active:
 		$ProgressBar.visible = true
 
 		start_tween = create_tween()
@@ -14,7 +22,7 @@ func _on_trigger_animation_body_entered(body: Node2D) -> void:
 		start_tween.connect('finished', tween_finished.bind(body))
 
 func _on_trigger_animation_body_exited(body: Node2D) -> void:
-	if body is Butterfly:
+	if body is Butterfly and is_active:
 		start_tween.kill()
 		var tween = create_tween()
 		tween.tween_property($ProgressBar, 'value', 0, 0.5)
@@ -27,4 +35,6 @@ func tween_reset():
 func tween_finished(player: Butterfly):
 	player.chroma_point += get_parent().chroma_point
 	$ProgressBar.visible = false
+	$GPUParticles2D.emitting = false
+	is_active = false
 	print("as")
