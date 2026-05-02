e45d2c84638384fd4a9644e85516e15689a54b19
JasperGMDV <sanikar3456@gmail.com>
2026-05-02 17:52:41 +0700

    bugfix

--- player/caterpillar/die.gd
+++ player/caterpillar/die.gd
@@ -5,9 +5,9 @@ extends CaterpillarState
 func enter_state():
 	#$"../../..".process_mode = Node.PROCESS_MODE_ALWAYS
 	#get_tree().paused = true
-	player.velocity.y -= 15
+	player.velocity.y += 5
 	
 	%CollisionShape2D.set_deferred("disabled", true)
 func update(delta: float):
 	$"../../../CanvasGroup".rotate(delta*2)
-	player.handle_gravity(delta*2.5)
+	player.handle_gravity(delta)
