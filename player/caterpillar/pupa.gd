ad3fa9a2e8d85edd93d2bc9dde693ae047d4e478
JasperGMDV <sanikar3456@gmail.com>
2026-05-03 16:57:01 +0700

    sound fixes

--- player/caterpillar/pupa.gd
+++ player/caterpillar/pupa.gd
@@ -3,8 +3,11 @@ extends CaterpillarState
 @onready var canvas_group: CanvasGroup = $"../../../CanvasGroup"
 @onready var caterpillar: Caterpillar = $"../.."
 
+const HURT = preload("uid://xf1pdjhwyjwo")
+
 
 func enter_state():
+	player.audio.stop()
 	caterpillar_container.global_position = caterpillar.global_position
 	caterpillar.position = Vector2.ZERO
 	canvas_group.position = Vector2.ZERO
@@ -12,8 +15,18 @@ func enter_state():
 		player.reset_scale()
 		await get_tree().create_timer(0.5).timeout
 		pupa()
+		await  get_tree().create_timer(0.6).timeout
+		player.audio.stream = HURT
+		player.audio.play()
+		await get_tree().create_timer(1.3).timeout
+		player.audio.play()
 	else:
 		pupa()
+		await  get_tree().create_timer(0.6).timeout
+		player.audio.stream = HURT
+		player.audio.play()
+		await get_tree().create_timer(1.3).timeout
+		player.audio.play()
 	#if player.current_state == states.stretch_right:
 		#player.change_state(states.move_right)
 		#await get_tree().create_timer(1).timeout
@@ -33,10 +46,13 @@ func pupa():
 	player.reset_tween()
 	player.tw.tween_property($"../../../CanvasGroup","scale:x", 1.25, 0.5)
 	player.tw.parallel().tween_property($"../../../CanvasGroup", "scale:y",0.5,0.5)
+	
+	
 	player.tw.tween_property($"../../../CanvasGroup", "scale:y",1.25,1)
 	player.tw.parallel().tween_property($"../../../CanvasGroup", "scale:x",0.5,1)
 	player.tw.tween_property($"../../../CanvasGroup", "scale:y",0.4,0.5)
 	player.tw.parallel().tween_property($"../../../CanvasGroup", "scale:x",1.6,0.5)
+
 	await get_tree().create_timer(1.8).timeout
 	$"../../../CanvasGroup".set_deferred("visible", false)
 	$"../../../pupa_sprite".global_position = player.global_position+Vector2(0, -50)
