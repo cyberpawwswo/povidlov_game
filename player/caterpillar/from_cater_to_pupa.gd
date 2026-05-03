808c5c2cedbec6542c5908c14d4fe93af4f8fa8c
JasperGMDV <sanikar3456@gmail.com>
2026-05-03 18:29:29 +0700

    small fixes

--- player/caterpillar/from_cater_to_pupa.gd
+++ player/caterpillar/from_cater_to_pupa.gd
@@ -3,5 +3,5 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	await get_tree().create_timer(5).timeout
+	await get_tree().create_timer(3).timeout
 	UI.change_level("res://player/cocoon/tutorial.tscn")
